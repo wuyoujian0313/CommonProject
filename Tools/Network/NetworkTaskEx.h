1d16383bb957c015ef9887332c7290a0c4f519e7
youjian.wu <youjian.wu@qunar.com>
2016-07-04 14:26:44 +0800

    增加一个支持AFNetworking3.0的封装通信类
    增加一个支持AFNetworking3.0的封装通信类

--- Tools/Network/NetworkTaskEx.h
+++ Tools/Network/NetworkTaskEx.h
@@ -1,9 +1,9 @@
 //
-//  NetworkTask.h
+//  NetworkTaskEx.h
+//  CommonProject
 //
-//
-//  Created by wuyj on 14-9-2.
-//  Copyright (c) 2014年 wuyj. All rights reserved.
+//  Created by wuyoujian on 16/7/4.
+//  Copyright © 2016年 wuyoujian. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -36,13 +36,11 @@ typedef NS_ENUM(NSInteger, NetStatusCode) {
 
 @end
 
-
-
-@interface NetworkTask : NSObject
+@interface NetworkTaskEx : NSObject
 
 @property(nonatomic, assign) NSTimeInterval taskTimeout;
 
-+ (NetworkTask *)sharedNetworkTask;
++ (NetworkTaskEx *)sharedNetworkTask;
 
 +(NSString *)errerDescription:(NSInteger)statusCode;
 
